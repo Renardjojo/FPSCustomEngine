14b2aaed142d890ee7c88367235f2d600e06ecdd
Jonathan <j.six@student.isartdigital.com>
2020-04-29 12:34:45 +0200

    Add the contructor and destructor implementation

---
+++ src/PhysicalObject.cpp
@@ -0,0 +1,34 @@
+#include "GE/Collision&Physics/PhysicalObject.hpp"
+
+using namespace Engine::Physics;
+
+PhysicalObject::PhysicalObject ()
+{
+
+}
+
+PhysicalObject::PhysicalObject (const PhysicalObject& other)
+{
+
+}
+
+PhysicalObject::PhysicalObject (PhysicalObject&& other)
+{
+
+}
+
+PhysicalObject::~PhysicalObject ()
+{
+
+}
+
+PhysicalObject& PhysicalObject::operator=(PhysicalObject const&)
+{
+
+}
+
+PhysicalObject& PhysicalObject::operator=(PhysicalObject &&)
+{
+
+
+}
