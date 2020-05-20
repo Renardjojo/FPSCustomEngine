b4aadb883eca409d90febfc7af51079563ee4500
Jonathan <j.six@student.isartdigital.com>
2020-05-20 20:45:44 +0200

    Add referential on scene.

--- include/Game/demo.hpp
+++ include/Game/demo.hpp
@@ -82,6 +82,7 @@ namespace Game
             void loadGround             (Engine::Ressources::t_RessourcesManager& ressourceManager);
             void loadSkateBoard         (Engine::Ressources::t_RessourcesManager& ressourceManager);
             void loadUI                 (Engine::Ressources::t_RessourcesManager& ressourceManager);
+            void loadReferential        (Engine::Ressources::t_RessourcesManager& ressourceManager);
             void loadCamera             ();
 
             void updateControl          ();
